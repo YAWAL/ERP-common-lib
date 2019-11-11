c2b1debf8cbd97d0e062bba3ae2eaf2662f2b889
YAWAL <yavorskyyval@gmail.com>
2019-11-11 23:45:24 +0200

    updated variables name in Mongo function

--- database/mongodb.go
+++ database/mongodb.go
@@ -12,18 +12,18 @@ import (
 const mongoDBstring = "mongodb://%v:%v"
 
 // Mongo creates client to MongoDB with host and port provided in database config file's part
-func Mongo(conf MongoConfig) (mongoCl *mongo.Client, err error) {
-	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI(conf.Host, conf.Port)))
+func Mongo(conf MongoConfig) (cl *mongo.Client, err error) {
+	cl, err = mongo.NewClient(options.Client().ApplyURI(mongoURI(conf.Host, conf.Port)))
 	if err != nil {
 		return nil, err
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	if err = client.Connect(ctx); err != nil {
+	if err = cl.Connect(ctx); err != nil {
 		return nil, err
 	}
-	return client, nil
+	return cl, nil
 }
 
 func mongoURI(host, port string) string {
